4f0ced76d22eef9efb6513abebee3403787f71fc
Katy Birkett <katybirkettlearns@gmail.com>
2021-02-27 16:43:23 +0100

    add form and values

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 $("h1").css("color", "blue");
+
+$("button").click(function () {
+    let stitchGauge = $("input[type=number][name=stitchGauge]").val();
+    let rowGauge = $("input[type=number][name=rowGauge]").val()
+    let diameter = $("input[type=number][name=diameter]").val()
+
+});
