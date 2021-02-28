9022519cd24048d598aca96c9c620c408cf15be2
Katy Birkett <katybirkettlearns@gmail.com>
2021-02-28 18:11:26 +0100

    add error checking

--- index.dev.js
+++ index.dev.js
@@ -4,30 +4,33 @@ $("button").click(function () {
   var stitchGauge = $("input[type=number][name=stitchGauge]").val();
   var rowGauge = $("input[type=number][name=rowGauge]").val();
   var diameter = $("input[type=number][name=diameter]").val();
-  var radius = diameter / 2;
-  var stitches = [];
   var rows = Math.round(diameter / 10 * rowGauge);
-  var cmPerRow = diameter / rows; //console.log(rows)
-  // for loop to spit out how many stitches to increase for each half of a semi circle
-  // pythagorus theorum
-  // hypotenuse = radius
-  // length = radius - current length of circle
-  // solve for width
 
-  var circleLength = cmPerRow;
+  if (rows < 3) {
+    $(".calculation").text("Cannot make a circle with those values");
+  } else {
+    var radius = diameter / 2;
+    var stitches = [];
+    var cmPerRow = diameter / rows; // for loop to spit out how many stitches to increase for each half of a semi circle
+    // pythagorus theorum
+    // hypotenuse = radius
+    // length = radius - current length of circle
+    // solve for width
 
-  for (var i = 0; i < rows - 1; i++) {
-    var l = radius - circleLength;
-    var width = Math.sqrt(radius * radius - l * l);
+    var circleLength = cmPerRow;
 
-    if (width > 0) {
-      var newStitches = Math.round(width / 10 * stitchGauge * 2);
-      stitches.push(newStitches);
+    for (var i = 0; i < rows - 1; i++) {
+      var l = radius - circleLength;
+      var width = Math.sqrt(radius * radius - l * l);
+
+      if (width > 0) {
+        var newStitches = Math.round(width / 10 * stitchGauge * 2);
+        stitches.push(newStitches);
+      }
+
+      circleLength = circleLength + cmPerRow;
     }
 
-    circleLength = circleLength + cmPerRow;
+    $(".calculation").text("You will need to knit " + rows + " rows. Here are the stitches needed for each of these rows to make a circle:" + stitches);
   }
-
-  $(".calculation").text("You will need to knit " + rows + " rows. Here are the stitches needed for each of these rows to make a circle:" + stitches);
-  console.log(stitches);
 });
